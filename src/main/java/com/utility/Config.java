4ddf802b18dfb934bcdcd9dd423d6916ec345add
your github username <your email id>
2023-06-07 18:46:33 +0530

    first commit

---
+++ src/main/java/com/utility/Config.java
@@ -0,0 +1,20 @@
+package com.utility;
+
+import org.openqa.selenium.WebDriver;
+
+
+public class Config {
+
+	public static WebDriver driver;
+	
+	public static String projectPath = System.getProperty("user.dir");
+	
+	public static String chromeDriverPath = projectPath + "\\drivers\\chromedriver.exe";
+	public static String geckoDriverPath = projectPath + "\\drivers\\geckodriver.exe";
+	
+	public static String screenshotsPath = projectPath + "\\screenshots\\";
+	
+	public static long PAGE_LOAD_TIMEOUT = 20;
+	public static long IMPLICIT_WAIT = 10;
+
+}
